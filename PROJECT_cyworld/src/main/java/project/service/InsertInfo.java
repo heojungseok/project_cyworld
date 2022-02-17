5946834f718ab2a1ba054616574247c520e03906
heojungseok <tjrwjdgj@gmail.com>
2022-02-18 01:00:23 +0900

    0218 edit

--- PROJECT_cyworld/src/main/java/project/service/InsertInfo.java
+++ PROJECT_cyworld/src/main/java/project/service/InsertInfo.java
@@ -2,6 +2,4 @@ package project.service;
 
 public interface InsertInfo {
 
-	public void insertContent(String userid, String userpw, String email, String username, String phoneNo, String address,
-			String gender);
 }
